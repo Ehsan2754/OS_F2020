36a95f8801444e2b8acceb6df3120acfd587ad35
ehsan2754 <ehsan2754@github.com>
2020-10-28 23:20:31 +0300

    ex4

--- week11/ex4.c
+++ week11/ex4.c
@@ -4,18 +4,27 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include <string.h>
+#include <unistd.h>
+#define PATH "./ex1.txt"
+#define TARGET "./ex1.memcry.txt"
 int main(int argc, char const *argv[])
 {
-    int fd = -1, parpid = getpid();
-    char *zero;
+    struct stat buffer_0;
+    int fd = -1, ft = -1, parpid = getpid();
+    char *zero, *one;
     if ((fd = open(PATH, O_RDWR, 0)) == -1)
-        err(1, "open");
-    zero = (char *)mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_FILE | MAP_SHARED, fd, 0);
+        err(1, "open %s",PATH);
+    fstat(fd, &buffer_0);
+    zero = mmap(NULL, buffer_0.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     if (zero == MAP_FAILED)
         errx(1, "either mmap");
-    strcpy(zero, text);
-    printf("PID %d:\t %s -> %s\n", parpid,PATH,zero);
+    if ((ft = open(TARGET, O_RDWR, 0)) == -1)
+        err(1, "open %s",TARGET);
+    ftruncate(ft, buffer_0.st_size);
+    one = mmap(NULL, buffer_0.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, ft, 0);
+    memcpy(one, zero, buffer_0.st_size);
+    printf("PID %d:\t ' %s -> %s ' :=: %s\n", parpid,PATH,TARGET,zero);
     close(fd);
+    close(ft);
     return (EXIT_SUCCESS);
-    return 0;
 }
